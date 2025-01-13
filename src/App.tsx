f73171e29f7c8c862cecd7031600e442c65e8af3
Teerut26 <teerutsr@gmail.com>
2025-01-13 22:50:22 +0700

    Add dotted grid option and adjust z-index for grid layers

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,15 @@ import { useEffect, useState } from "react"
 export default function App() {
     const [horizontalLines, setHorizontalLines] = useState<number>(1)
     const [verticalLines, setVerticalLines] = useState<number>(3)
+    const [showDotted, setShowDotted] = useState<boolean>(false)
     const onPrint = () => {
         window.print()
     }
 
     useEffect(() => {
-      document.title = `Grid: ${horizontalLines}x${verticalLines}`
+        document.title = `Grid: ${horizontalLines}x${verticalLines}`
     }, [horizontalLines, verticalLines])
-    
+
 
     return (
         <>
@@ -36,23 +37,35 @@ export default function App() {
                             onChange={(e) => setVerticalLines(Number(e.target.value) < 1 ? 1 : Number(e.target.value))}
                         />
                     </div>
+                    <div className="flex flex-col items-start">
+                        <span>Show dotted grid:</span>
+                        <input
+                            type="checkbox"
+                            className="border-2 p-2 rounded-md"
+                            checked={showDotted}
+                            onChange={(e) => setShowDotted(e.target.checked)}
+                        />
+                    </div>
                     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded h-fit" onClick={onPrint}>Print</button>
                 </div>
             </div>
             <div className="print:h-screen print:w-full h-screen w-full relative border mx-3 print:m-0 print:border-0 border-gray-300">
                 {/* horizontal grid */}
-                <div className={clsx("absolute top-0 bottom-0 left-0 right-0 z-10 flex flex-col justify-evenly")}>
+                <div className={clsx("absolute top-0 bottom-0 left-0 right-0 z-20 flex flex-col justify-evenly")}>
                     {[...Array(horizontalLines)].map((_, i) => (
                         <div className="h-[0.5px] w-full bg-gray-500 print:bg-gray-200" key={i}></div>
                     ))}
                 </div>
 
                 {/* vertical grid */}
-                <div className="absolute top-0 bottom-0 left-0 right-0 z-20 flex items-center justify-evenly">
+                <div className="absolute top-0 bottom-0 left-0 right-0 z-30 flex items-center justify-evenly">
                     {[...Array(verticalLines)].map((_, i) => (
                         <div className="w-[0.5px] h-full bg-gray-500 print:bg-gray-200" key={i}></div>
                     ))}
                 </div>
+
+                {/* Dotted grid */}
+                {showDotted && <div className="absolute top-0 bottom-0 left-0 right-0 z-10 flex items-center justify-evenly dotted"></div>}
             </div>
         </>
     )
